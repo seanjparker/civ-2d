325d579b1b3d942063391636061e6c91dac7e6ef
Sean Parker <sean.parker067@gmail.com>
2017-07-07 17:42:45 +0100

    Added new noise generation algorithm

--- src/com/proj/civ/map/generation/TerrainGeneration.java
+++ src/com/proj/civ/map/generation/TerrainGeneration.java
@@ -1,57 +1,38 @@
 package com.proj.civ.map.generation;
 
-import java.util.Random;
-
 public class TerrainGeneration {
-	private final double PERSISTANCE = 0.3;
-	private final int OCTAVES = 2;
-	private final double PEAK_ELEVATION = 3.35;
-	private final double SEED_ELEVATION, SEED_MOISTURE;
-	private final double INCREMENT = 0.003;
 	
-	private double xOff, yOff;
-	private int xS, yS;
+	private int width, height;
+	private double hexWidth, hexHeight;
+	
 	private double[] value;
 	private Noise n;
-	public TerrainGeneration(int widthCell, int heightCell) {
-		this.xS = widthCell;
-		this.yS = heightCell;
-		this.value = new double[xS * yS * 6];
-		this.SEED_ELEVATION = generateSeed();
-		this.SEED_MOISTURE = generateSeed();
+	public TerrainGeneration(int width, int height, int hexWidth, int hexHeight) {
+		this.value = new double[width * height];
+		this.width = width;
+		this.height = height;
+		this.hexWidth = hexWidth;
+		this.hexHeight = hexHeight;
+		
 		n = new Noise();
 	}
 	
 	public double[] generateElevation() {
-		xOff = 0;
-		yOff = 0;
-		for (int yi = 0; yi < yS; yi++) {
-			xOff = 0;
-			for (int xi = 0; xi < xS; xi++) {
-				int i = xi + yi * yS;
-				//value[i] = n.elevationNoise(xOff, yOff, OCTAVES, PERSISTANCE, SEED_ELEVATION, PEAK_ELEVATION);
-				xOff += INCREMENT;
+		for (int yi = 0; yi < height; yi++) {
+			for (int xi = 0; xi < width; xi++) {
+				int i = xi + yi * height;
+				value[i] = n.eval(xi / hexWidth, yi / hexHeight);
 			}
-			yOff += INCREMENT;
 		}		
 		return value;
 	}
 	public double[] generateMoisture() {
-		xOff = 0;
-		yOff = 0;
-		for (int yi = 0; yi < yS; yi++) {
-			for (int xi = 0; xi < xS; xi++) {
+		//for (int yi = 0; yi < yS; yi++) {
+		//	for (int xi = 0; xi < xS; xi++) {
 				//value[xi + yi * yS] = n.moistureNoise(xOff, yOff, OCTAVES, PERSISTANCE, SEED_MOISTURE);
 			
-				xOff += INCREMENT;
-			}
-			yOff += INCREMENT;
-		}		
+		//	}
+		//}		
 		return value;
-	}
-	
-	private double generateSeed() {
-		Random rnd = new Random();
-		return rnd.nextDouble();
 	}
 }
