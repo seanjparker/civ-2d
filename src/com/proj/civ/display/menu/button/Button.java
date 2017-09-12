295c695d778ada6e163ef6ba4f593460c85bf0ab
seanparker <seanparker@192.168.0.32>
2017-09-12 21:57:35 +0100

    Added images to buttons, buttons rendered with images

--- src/com/proj/civ/display/menu/button/Button.java
+++ src/com/proj/civ/display/menu/button/Button.java
@@ -1,8 +1,10 @@
 package com.proj.civ.display.menu.button;
 
+import java.awt.Image;
 import java.awt.Rectangle;
 
 import com.proj.civ.event.ButtonEventHandler;
+import com.proj.civ.event.Events;
 import com.proj.civ.instance.IData;
 
 public abstract class Button extends IData implements ButtonEventHandler {
@@ -12,14 +14,15 @@ public abstract class Button extends IData implements ButtonEventHandler {
 	protected boolean isClickable;
 	
 	protected Rectangle buttonBounds;
+	protected Events e = null;
 	
 	public Button(int menuWidth, int menuHeight, int menuButtonIndex, boolean isClickable) {
-		this.buttonSizeX = menuWidth >> 1;
-		this.buttonSizeY = menuWidth >> 1;
-		this.bufferX = menuWidth >> 2;
-		this.bufferY = bufferX;
+		this.buttonSizeX = (menuWidth / 4) * 3;
+		this.buttonSizeY = buttonSizeX;
+		this.bufferX = menuWidth / 2;
+		this.bufferY = bufferX / 2;
 		this.isClickable = isClickable;
-		this.xPos = bufferX;
+		this.xPos = bufferX - (buttonSizeX / 2);
 		this.yPos = (HEIGHT - menuHeight + bufferY) + (menuButtonIndex * (this.buttonSizeY + bufferY));
 		this.buttonIndex = menuButtonIndex;
 		
@@ -40,5 +43,54 @@ public abstract class Button extends IData implements ButtonEventHandler {
 	}
 	public void setIsClickable(boolean isClickable) {
 		this.isClickable = isClickable;
+	}
+	
+	public void performEvent() {
+		if (e != null) {
+			switch (e) {
+			case FOUND_CITY:
+				System.out.println("Found City");
+				break;
+			case MOVE:
+				System.out.println("Move");
+				break;
+			case ATTACK:
+				System.out.println("Attack");
+				break;
+			case AUTO_EXPLORE:
+				System.out.println("Auto Explore");
+				break;
+			case DO_NOTHING:
+				System.out.println("Do Nothing");
+				break;
+			case SLEEP:
+				System.out.println("Sleep");
+				break;
+			case DELETE:
+				System.out.println("Delete");
+				break;
+			case NEXT_TURN:
+				System.out.println("Next Turn");
+				ui.nextTurn();
+				break;
+			case CIVILOPEDIA_OPEN:
+				System.out.println("Open Civilopedia");
+				break;
+			case RESEARCH_TREE_OPEN:
+				System.out.println("Open Research Tree");
+				break;
+			case CITY_OVERVIEW_OPEN:
+				System.out.println("Open City Overview");
+				break;
+			case CITY_PRODUCTION_OPEN:
+				System.out.println("Open City Production");
+				break;
+			case CULTURE_TREE_OPEN:
+				System.out.println("Open Culture Tree");
+				break;
+			default:
+				break;
+			}
+		}
 	}
 }
--- src/com/proj/civ/display/menu/button/UnitMenuButton.java
+++ src/com/proj/civ/display/menu/button/UnitMenuButton.java
@@ -1,27 +1,28 @@
 package com.proj.civ.display.menu.button;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 
+import com.proj.civ.event.Events;
 import com.proj.civ.input.MouseHandler;
 
 public class UnitMenuButton extends Button{
 
-	public UnitMenuButton(int bIndex, boolean isClickable) {
+	public UnitMenuButton(Events e, int bIndex, boolean isClickable) {
 		super(HEX_RADIUS, HEX_RADIUS * 4, bIndex, isClickable);
+		this.e = e;
 	}
 
 	public void drawButton(Graphics2D g) {
-		g.setColor(Color.WHITE);
-		g.fill3DRect(xPos, yPos, buttonSizeX, buttonSizeY, isClickable);
+		//g.setColor(Color.WHITE);
+		//g.fill3DRect(xPos, yPos, buttonSizeX, buttonSizeY, isClickable);
+		g.drawImage(e.getImage(), xPos, yPos, buttonSizeX, buttonSizeY, null);
 	}
 
 	public void onPress() {
 		if (MouseHandler.pressedMouse) {
 			if (buttonBounds.intersects(MouseHandler.mX, MouseHandler.mY, BUTTON_CLICK_BUFFER, BUTTON_CLICK_BUFFER)) {
 				MouseHandler.pressedMouse = false;
-				
-				//Code for the button click action
+				performEvent();
 			}
 		}
 	}
