41ce4ae1a9111104053e8a50107476d1a08e52aa
Sean Parker <sean.parker067@gmail.com>
2018-06-25 00:30:32 +0100

    Add timer, unlinked render and updates

--- src/civ/core/Civilization.java
+++ src/civ/core/Civilization.java
@@ -4,7 +4,9 @@ import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import org.lwjgl.system.*;
+import civ.core.timing.Timer;
 import java.nio.*;
+
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
@@ -12,12 +14,14 @@ import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class Civilization {
-
-  // The window handle
-  private long window;
+  private final String TITLE = "Civilization";
+  private final double TARGET_UPS = 30D;
+  
+  private long window; // The window handle
+  boolean resizable = true; // Whether or not the window is resizable
 
   public void run() {
-    System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+    System.out.println("LWJGL " + Version.getVersion() + "");
 
     init();
     loop();
@@ -42,13 +46,25 @@ public class Civilization {
 
     // Configure GLFW
     glfwDefaultWindowHints(); // optional, the current window hints are already the default
+    
+    //When we are running on macOS, we need specific settings
+    String lcOSName = System.getProperty("os.name");
+    boolean IS_MAC = lcOSName.contains("OS X");
+    if (IS_MAC) {
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+      glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    }
+    
     glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
+    glfwWindowHint(GLFW_RESIZABLE, resizable ? GLFW_TRUE : GLFW_FALSE); // the window will be resizable
 
     // Create the window
-    window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+    window = glfwCreateWindow(300, 300, TITLE, NULL, NULL);
     if (window == NULL)
       throw new RuntimeException("Failed to create the GLFW window");
+
 
     // Setup a key callback. It will be called every time a key is pressed, repeated or released.
     glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
@@ -79,9 +95,7 @@ public class Civilization {
 
     // Make the window visible
     glfwShowWindow(window);
-  }
-
-  private void loop() {
+    
     // This line is critical for LWJGL's interoperation with GLFW's
     // OpenGL context, or any context that is managed externally.
     // LWJGL detects the context that is current in the current thread,
@@ -89,20 +103,58 @@ public class Civilization {
     // bindings available for use.
     GL.createCapabilities();
 
-    // Set the clear color
-    glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    // Set the clear color to light gray
+    glClearColor(0.25f, 0.25f, 0.25f, 0.0f);
+  }
 
+  private void loop() {    
+    Timer t = new Timer();
+    t.init();
+    
+    double delta;
+    double accumulator = 0D;
+    double interval = 1D / TARGET_UPS;
+    
     // Run the rendering loop until the user has attempted to close
     // the window or has pressed the ESCAPE key.
     while (!glfwWindowShouldClose(window)) {
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-
-      glfwSwapBuffers(window); // swap the color buffers
-
       // Poll for window events. The key callback above will only be
       // invoked during this call.
       glfwPollEvents();
+      
+      delta = t.getDelta();
+      accumulator += delta;
+      
+      //input();
+      
+      while (accumulator >= interval) {
+        update();
+        t.incrementUPS();
+        accumulator -= interval;
+      }
+      
+      render();
+      t.incrementFPS();
+      t.update();
+     
+      glfwSwapBuffers(window); // swap the color buffers
     }
+  }
+  
+  private void render() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+    
+    //glBegin(GL_POLYGON);
+    //for(int i = 0; i < 6; ++i) {
+    //    glVertex2d(Math.sin(i/6.0*2*Math.PI),
+    //               Math.cos(i/6.0*2*Math.PI));
+   // }
+    //glEnd();
+    
+  }
+  
+  private void update() {
+    
   }
 
   public static void main(String[] args) {
---
+++ src/civ/core/timing/Timer.java
@@ -0,0 +1,138 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright © 2014-2015, Heiko Brumme
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package civ.core.timing;
+
+import static org.lwjgl.glfw.GLFW.glfwGetTime;
+
+public class Timer {
+  private double lastLoopTime;
+  /**
+   * Used for FPS and UPS calculation.
+   */
+  private double timeCount;
+  /**
+   * Frames per second.
+   */
+  private int fps;
+  /**
+   * Counter for the FPS calculation.
+   */
+  private int fpsCount;
+  /**
+   * Updates per second.
+   */
+  private int ups;
+  /**
+   * Counter for the UPS calculation.
+   */
+  private int upsCount;
+
+  /**
+   * Initializes the timer.
+   */
+  public void init() {
+      lastLoopTime = getTime();
+  }
+
+  /**
+   * Returns the time elapsed since <code>glfwInit()</code> in seconds.
+   *
+   * @return System time in seconds
+   */
+  public double getTime() {
+      return glfwGetTime();
+  }
+
+  /**
+   * Returns the time that have passed since the last loop.
+   *
+   * @return Delta time in seconds
+   */
+  public double getDelta() {
+      double time = getTime();
+      double delta = time - lastLoopTime;
+      lastLoopTime = time;
+      timeCount += delta;
+      return delta;
+  }
+
+  /**
+   * Updates the FPS counter.
+   */
+  public void incrementFPS() {
+      fpsCount++;
+  }
+
+  /**
+   * Updates the UPS counter.
+   */
+  public void incrementUPS() {
+      upsCount++;
+  }
+
+  /**
+   * Updates FPS and UPS if a whole second has passed.
+   */
+  public void update() {
+      if (timeCount > 1f) {
+        System.out.println("FPS =" + fpsCount + "| UPS =" + upsCount);
+        fps = fpsCount;
+        fpsCount = 0;
+
+        ups = upsCount;
+        upsCount = 0;
+
+        timeCount -= 1f;
+      }
+  }
+
+  /**
+   * Getter for the FPS.
+   *
+   * @return Frames per second
+   */
+  public int getFPS() {
+      return fps > 0 ? fps : fpsCount;
+  }
+
+  /**
+   * Getter for the UPS.
+   *
+   * @return Updates per second
+   */
+  public int getUPS() {
+      return ups > 0 ? ups : upsCount;
+  }
+
+  /**
+   * Getter for the last loop time.
+   *
+   * @return System time of the last loop
+   */
+  public double getLastLoopTime() {
+      return lastLoopTime;
+  }
+
+}
